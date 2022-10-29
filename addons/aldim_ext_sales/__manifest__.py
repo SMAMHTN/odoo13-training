c9eb153de7e0b6267d29c5aefeda11b16cd65dc3
SMAM <aldismartkid@gmail.com>
2022-10-29 22:40:15 +0700

    training

--- addons/aldim_ext_sales/__manifest__.py
+++ addons/aldim_ext_sales/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'Sales Extended AldiM',
-    'version': '1.1',
+    'version': '1.2',
     'category': 'Sales/Sales',
     'depends': ['base','sale'],
     'description': """
@@ -11,7 +11,8 @@ Modul sales dengan adanya tambahan end_user sebagai tujuan akhir
 penerima barang.
     """,
     'data': [
-        'views/aldim_ext_sales_view.xml'
+        'views/aldim_ext_sales_view.xml',
+        'report/aldim_ext_sales_report.xml'
     ],
     'installable': True,
     'auto_install': False,
